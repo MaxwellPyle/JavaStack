87a4c73532a21111c5ddc86b802bfad77deb919a
Maxwell Pyle <86977878+MaxwellPyle@users.noreply.github.com>
2022-02-07 17:28:32 -0500

    Add files via upload

---
+++ JavaStack/Java Fundamentals/FirstProgram/Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction{
+    public static void main(String[] args){
+        System.out.println("Hello, my name is Max");
+        System.out.println("I am 23 years old");
+        System.out.println("I Live in Northern Virginia");
+    }
+}
